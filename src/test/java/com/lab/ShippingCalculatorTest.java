f4a17fe6c69596922b84cc673a0bd354e3d321a5
fushi <kaiki0201a@gmail.com>
2026-05-06 22:35:19 +0700

    Loi cua bai

---
+++ src/test/java/com/lab/ShippingCalculatorTest.java
@@ -0,0 +1,20 @@
+public class ShippingCalculatorTest {
+
+    ShippingCalculator calc = new ShippingCalculator();
+
+    @Test
+    void testStandard() {
+        assertEquals(15000.0, calc.calculate(5, "STANDARD"));
+    }
+
+    @Test
+    void testExpress() {
+        assertEquals(45000.0, calc.calculate(5, "EXPRESS"));
+    }
+
+    @Test
+    void testInvalidWeight() {
+        assertThrows(IllegalArgumentException.class,
+                () -> calc.calculate(-1, "STANDARD"));
+    }
+}
